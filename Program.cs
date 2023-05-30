c96da5892dbe3ccf70b92d9d59fe099cdd945164
c2056love <66960217+c2056love@users.noreply.github.com>
2023-05-30 10:14:24 +0800

    Test
    testfile

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace TestXceedDocx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region XceedDoc
+            FileStream fs_1 = new FileStream("../../1.docx", FileMode.Open);
+            DocX doc_1 = DocX.Load(fs_1);
+            FileStream fs_2 = new FileStream("../../2.docx", FileMode.Open);
+
+            DocX doc_2 = DocX.Load(fs_2);
+
+            doc_1.InsertDocument(doc_2, true, true, mergingMode: MergingMode.Remote);
+
+            try
+            {
+                doc_1.SaveAs("../../result.docx");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            #endregion
+        }
+    }
+}
